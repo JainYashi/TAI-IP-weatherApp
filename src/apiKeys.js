1861e4f10aae65d9963a2d9d6e87a74e46e12672
JainYashi <jainy5089@gmail.com>
2023-07-17 18:44:55 +0530

    Final changes

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "88ec858cd6cb5e773e4c68a3940d2152",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
